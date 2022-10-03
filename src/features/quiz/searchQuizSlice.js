392c636051b07cd01b22429379789f6317858713
khanhtv1999 <khanh1999ns@gmail.com>
2022-10-04 01:32:40 +0700

    commit

--- src/features/quiz/searchQuizSlice.js
+++ src/features/quiz/searchQuizSlice.js
@@ -1,33 +1,64 @@
 const initialFiltersState = {
   search: "",
-  searchType: "Title",
+  searchType: "All",
   sort: "Ascending",
   sortOptions: ["Ascending", "Descending"],
 };
 const initialState = {
   isLoading: true,
   quizs: [],
-  pageSize: 8,
+  pageSize: 10,
   currentPage: 1,
+  totalPages: 0,
+  totalQuiz: 0,
   ...initialFiltersState,
 };
-const allJobsSlice = createSlice({
+export const getAllQuiz = createAsyncThunk("allJobs/getJobs", getAllQuizThunk);
+const searchQuizSlice = createSlice({
   name: "searchQuiz",
   initialState,
   reducers: {
     showLoading: (state) => {
       state.isLoading = true;
     },
+    hideLoading: (state) => {
+      state.isLoading = false;
+    },
+    handleChange: (state, { payload: { name, value } }) => {
+      state.currentPage = 1;
+      state[name] = value;
+    },
+    clearFilters: (state) => {
+      return { ...state, ...initialFiltersState };
+    },
+    changePage: (state, { payload }) => {
+      state.currentPage = payload;
+    },
+    clearAllQuizState: (state) => initialState,
   },
-  handleChange: (state, { payload: { name, value } }) => {
-    state.page = 1;
-    state[name] = value;
+
+  extraReducers: {
+    [getAllQuiz.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getAllQuiz.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.quizs = payload?.result;
+      state.currentPage = payload.currentPage;
+      state.totalPages = payload.totalPages;
+      state.totalQuiz = state.total;
+    },
+    [getAllQuiz.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+    },
   },
-  clearFilters: (state) => {
-    return { ...state, ...initialFiltersState };
-  },
-  changePage: (state, { payload }) => {
-    state.page = payload;
-  },
-  clearAllJobsState: (state) => initialState,
 });
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+  clearAllQuizState,
+} = searchQuizSlice.actions;
+export default searchQuizSlice;
--- src/features/quiz/searchQuizThunk.js
+++ src/features/quiz/searchQuizThunk.js
@@ -1,1 +1,15 @@
+import customFetch from "../../utils/axiosCustomize";
+export const getAllQuizThunk = async (_, thunkAPI) => {
+  const { totalPages, search, searchType, sort } = thunkAPI.getState().allJobs;
 
+  let url = `/questions?oder=${sort}&sortField=${searchType}&sort=${sort}&page=${totalPages}`;
+  if (search) {
+    url = url + `&keyWord=${search}`;
+  }
+  try {
+    const resp = await customFetch.get(url);
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
